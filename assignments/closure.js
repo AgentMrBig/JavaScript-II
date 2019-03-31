d12f6009696942b3e88fec05a95e660c92176b59
Eric Hendrix <agentmrbig@gmail.com>
2019-03-30 20:03:49 -0400

    Final commit, closure.js done, one stretch challenge done

--- assignments/closure.js
+++ assignments/closure.js
@@ -19,25 +19,32 @@ all previous tasks for today's project files */
 
 
 // ==== Challenge 2: Create a counter function ====
-const counter = (interval) => {
+
+const counter = (interval, name) => {
   // Return a function that when invoked increments and 
   // returns a counter variable.
-  
-  var counter = 0;
+  var cnt = 0;
   setInterval(timeIt, interval);
+
+  // timeIt function can take a function as argument for either
+  // increment or decrement
   function timeIt(){
-    counter++;
+    console.log(name + cnt);
+    cnt++;
   }
+
+  
 };
 
 
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
-counter(500);
 
-// ==== Challenge 3: Create a counter function with an object 
-// that can increment and decrement ====
+//counter(500, "c one ");
+//counter(2000, "c two ");
+
+// ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
 const counterFactory = () => {
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
